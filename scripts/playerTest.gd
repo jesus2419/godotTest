7c720757be429a932471f4af928d4d1822bc5a4b
jesus2419 <102651007+jesus2419@users.noreply.github.com>
2024-07-19 17:12:02 -0600

    0.2
    movimiento y gravedad

---
+++ scripts/playerTest.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+const moveSpeed = 25
+const maxSpeed = 50
+const jumpHeight = -400
+const gravity = 15
+
+@onready var sprite = $Sprite2D
+@onready var animationPlayer = $AnimationPlayer
+
+
+
+func _physics_process(_delta):
+	velocity.y += gravity
+	var friction = false
+
+	if Input.is_action_pressed("ui_right"):
+		sprite.flip_h = true
+		animationPlayer.play("walk")
+		velocity.x = min(velocity.x + moveSpeed, maxSpeed)
+
+	elif Input.is_action_pressed("ui_left"):
+		sprite.flip_h = false
+		animationPlayer.play("walk")
+		velocity.x = max(velocity.x - moveSpeed, -maxSpeed)
+
+	else:
+		animationPlayer.play("idle")
+		friction = true
+
+	if is_on_floor():
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = jumpHeight
+		if friction:
+			velocity.x = lerp(velocity.x, 0.0, 0.5)
+	else:
+		if friction:
+			velocity.x = lerp(velocity.x, 0.0, 0.01)
+
+	move_and_slide()
+
+
+
+
